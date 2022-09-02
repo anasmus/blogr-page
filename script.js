2e820203635fdd79cc3b67b5eb409ca4eea4679b
Anas Mustafa <anasmtfa@gmail.com>
2022-09-02 10:57:37 +0500

    Add Hamburger Menu for Mobile Layout

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ document.querySelectorAll('.main-nav-item').forEach(item => {
 document.querySelectorAll('.secondary-nav li').forEach(li => {
     li.addEventListener('click', hideOpenedList)
 });
+document.querySelector('.hamburger').addEventListener('click', menuOpen);
 function navClickHandler(e) {
     const targetId = e.target.id;
     hideOpenedList(targetId);
+    // class active is used to display correct arrow next to nav items
+    document.getElementById(targetId).classList.toggle('active');
     document.querySelector(`#${targetId} + .secondary-nav`).classList.toggle('hidden');
 }
 
 function hideOpenedList(targetId) {
     navbarItemIds.forEach(itemId => {
         if (itemId !== targetId) {
+            document.getElementById(itemId).classList.remove('active');
             document.querySelector(`#${itemId} + .secondary-nav`).classList.add('hidden');
         }
     });
+}
+function menuOpen(e) {
+    if (e.target.getAttribute('src') == 'images/icon-hamburger.svg') {
+        e.target.setAttribute('src', 'images/icon-close.svg');
+    } else {
+        e.target.setAttribute('src', 'images/icon-hamburger.svg');
+    }
+    document.querySelector('.navbar').classList.toggle('mobile-menu');
 }
